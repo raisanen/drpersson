151cc9d1441e994f02938d8e490e33ff87cd4865
Kalle <kr@impera.se>
2019-04-08 12:06:41 +0200

    Refactor

--- App.js
+++ App.js
@@ -1,31 +1,12 @@
 import React, { PureComponent } from 'react';
-import { StyleSheet, Dimensions, Text, View, TouchableHighlight, Image } from "react-native";
-import { GameLoop } from "react-native-game-engine";
-import { Font, Audio } from 'expo';
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+import { GameLoop } from 'react-native-game-engine';
+import { Font } from 'expo';
 import Matter from "matter-js";
 
-const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
-const RADIUS = 12;
-const MAXSCORE = 12;
-const WINNINGSCORE = MAXSCORE / 2;
-const VALIDCOLORS = ['#fff', '#000', '#f00'];
-
-const INITIALGRAVITY = 0.1;
-const DELTASPEED = 1.1;
-
-const PILLwIDTH = RADIUS * 5;
-const PILLHEIGHT = RADIUS * 2;
-
-const BEAKERWIDTH = WIDTH / 3;
-const BEAKERHEIGHT = BEAKERWIDTH * 1.105;
-const REALBEAKERWIDTH = (BEAKERWIDTH * 0.9)
-const LEFTCOLLIDERWIDTH = 10;
-const LEFTCOLLIDERHEIGHT = BEAKERHEIGHT;
-const RIGHTCOLLIDERWITH = (BEAKERWIDTH - REALBEAKERWIDTH) * 2;
-const BOTTOMCOLLIDERWIDTH = REALBEAKERWIDTH;
-const BOTTOMCOLLIDERHEIGHT = LEFTCOLLIDERWIDTH;
-
-const perssonRatio = (WIDTH / 4) / 406;
+import { colliders, Pill, Beaker, Persson, Ground, makePill, randomColors } from './src/components';
+import { PerssonPlayer } from './src/sound';
+import { WIDTH, HEIGHT } from './src/constants';
 
 const styles = StyleSheet.create({
   container: {
@@ -33,44 +14,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#ff0',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  persson: {
-    position: 'absolute',
-    left: WIDTH / 2 - (406 * perssonRatio) / 2,
-    bottom: 50,
-    width: 406 * perssonRatio,
-    height: 587 * perssonRatio
-  },
-  perssonHead: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    width: 406 * perssonRatio,
-    height: 395 * perssonRatio,
-  },
-  personBody: {
-    position: 'absolute',
-    top: 300 * perssonRatio,
-    left: 35 * perssonRatio,
-    width: 356 * perssonRatio,
-    height: 265 * perssonRatio,
-  },
-  personEyebrow: {
-    position: 'absolute',
-    top: 160 * perssonRatio,
-    left: 225 * perssonRatio,
-    width: 103 * perssonRatio,
-    height: 20 * perssonRatio
-  },
-  absolute: {
-    position: 'absolute',
-  },
-  beaker: {
-    height: BEAKERHEIGHT,
-    width: BEAKERWIDTH,
-    position: 'absolute',
-    bottom: 20,
-    right: 20
   },
   text: {
     color: '#000',
@@ -95,49 +38,8 @@ const styles = StyleSheet.create({
     fontSize: 32,
     fontFamily: 'space-grotesk-bold'
   },
-  pill: {
-    borderRadius: RADIUS * 2,
-    width: PILLwIDTH,
-    height: PILLHEIGHT,
-    backgroundColor: "transparent",
-    position: 'absolute',
-  },
-  side: {
-    position: "absolute",
-    top: 0,
-    width: RADIUS * 2.5,
-    height: RADIUS * 2,
-  },
-  left: {
-    left: 0,
-    borderTopLeftRadius: RADIUS,
-    borderBottomLeftRadius: RADIUS,
-  },
-  right: {
-    right: 0,
-    borderTopRightRadius: RADIUS,
-    borderBottomRightRadius: RADIUS,
-  },
 });
 
-const randomColor = () => VALIDCOLORS[Math.floor(Math.random() * VALIDCOLORS.length)];
-const randomX = () => Math.floor(Math.random() * (WIDTH - (WIDTH / 4) - 100));
-const makePill = (density = 0.001) => {
-  return Matter.Bodies.rectangle(
-    randomX(),
-    0,
-    PILLwIDTH,
-    PILLHEIGHT,
-    {
-      inertia: 0,
-      friction: 0.6,
-      restitution: 1,
-      chamfer: { radius: RADIUS },
-      label: 'pill',
-      density: density,
-    }
-  );
-};
 const numToText = (n) => {
   const uptonineteen = [
       'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
@@ -152,65 +54,16 @@ const numToText = (n) => {
   return (n < translate.length) ? translate[n] : 'infinite';
 };
 
-
-const colliderSettings = { isStatic: true, friction: 0.2, restitution: 1, label: 'collider' };
-const colliderLeft = Matter.Bodies.rectangle(
-  WIDTH - BEAKERWIDTH - styles.beaker.right + (LEFTCOLLIDERWIDTH * 4), (HEIGHT - BEAKERHEIGHT - styles.beaker.bottom) + LEFTCOLLIDERHEIGHT / 2, LEFTCOLLIDERWIDTH, LEFTCOLLIDERHEIGHT,
-  { ...colliderSettings }
-);
-const colliderRight = Matter.Bodies.rectangle(
-  WIDTH - styles.beaker.right - (BEAKERWIDTH - REALBEAKERWIDTH) + (RIGHTCOLLIDERWITH / 2), (HEIGHT - BEAKERHEIGHT - styles.beaker.bottom) + LEFTCOLLIDERHEIGHT / 2, RIGHTCOLLIDERWITH, LEFTCOLLIDERHEIGHT,
-  { ...colliderSettings }
-);
-const colliderBottom = Matter.Bodies.rectangle(
-  WIDTH - BEAKERWIDTH - styles.beaker.right + (BOTTOMCOLLIDERWIDTH / 2), HEIGHT - styles.beaker.bottom, BOTTOMCOLLIDERWIDTH, BOTTOMCOLLIDERHEIGHT,
-  { ...colliderSettings, restitution: 0, label: 'beaker' }
-);
-const ground = Matter.Bodies.rectangle(
-  WIDTH / 2, HEIGHT - styles.beaker.bottom + 15 + HEIGHT, WIDTH * 50, HEIGHT * 2,
-  { isStatic: true, label: 'ground' }
-);
-
+// Init engine
 const engine = Matter.Engine.create(),
   world = engine.world,
   runner = Matter.Runner.create();
 
 engine.world.gravity = { x: 0, y: 0 };
 
-class Pill extends PureComponent {
-  render() {
-    const { body, colors } = this.props;
-    return !(body && colors) ? null : (
-      <View style={[styles.pill, { top: body.position.y - PILLHEIGHT, left: body.position.x - PILLwIDTH }]}>
-        <View style={[styles.side, styles.left, { backgroundColor: colors.left }]} />
-        <View style={[styles.side, styles.right, { backgroundColor: colors.right }]} />
-      </View>
-    );
-  }
-}
+colliders.forEach((c) => Matter.World.addBody(c));
+Matter.Runner.run(runner, engine);
 
-class Beaker extends PureComponent {
-  render() {
-    return (
-      <Image
-        style={[styles.beaker]}
-        source={require('./assets/images/beaker.png')} />
-    );
-  }
-}
-
-class Persson extends PureComponent {
-  render() {
-    const { offset, eyebrow } = this.props;
-    return (
-      <View style={[styles.persson]}>
-        <Image style={[styles.personBody]} source={require('./assets/images/pbody.png')} />
-        <Image style={[styles.perssonHead, offset]} source={require('./assets/images/phead.png')} />
-        <Image style={[styles.personEyebrow, eyebrow]} source={require('./assets/images/eyebrow.png')} />
-      </View>
-    )
-  }
-}
 
 const GAMESTATES = {
   init: 0,
@@ -229,6 +82,8 @@ export default class App extends PureComponent {
     personOffset: {},
     personEyebrow: {}
   };
+  
+  soundPlayer = new PerssonPlayer();
 
   constructor(props) {
     super(props);
@@ -239,154 +94,31 @@ export default class App extends PureComponent {
       }
       const active = world.bodies.filter(b => b.label === 'pill');
       if (active && active.length > 0) {
-        let deltaX = 0;
-
         const move = touches.find(x => x.type === "move");
+        const deltaX = move ? move.delta.pageX : 0;
 
-        if (move) {
-          deltaX = move.delta.pageX;
-
-          active.forEach((b) => {
-            if (!b.isStatic && !b.isSleeping) {
-              if (b.position.y > HEIGHT) {
-                this.removePill(b);
-              } else {
-                Matter.Body.setVelocity(
-                  b, {
-                    x: deltaX,
-                    y: b.velocity.y
-                  }
-                );
-              }
+        active.forEach((b) => {
+          if (!b.isStatic && !b.isSleeping) {
+            if (b.position.y > HEIGHT) {
+              this.removePill(b);
+            } else if (move) {
+              Matter.Body.setVelocity(
+                b, {
+                  x: deltaX,
+                  y: b.velocity.y
+                }
+              );
             }
-          });
-        }
+          }
+        });
       }
 
       Matter.Engine.update(engine, time.delta);
     };
   };
 
-  removePill = (body) => {
-    this.setState({ pills: this.state.pills.filter(p => p.body.id !== body.id) });
-    Matter.World.remove(world, body);
-  };
-
-  init = () => {
-    world.gravity.y = INITIALGRAVITY;
-
-    this.setState({
-      gameState: GAMESTATES.started,
-      pills: [],
-      pillsLeft: MAXSCORE,
-      score: 0
-    });
-
-    setTimeout(this.updatePills, 250);
-  };
-
-  start = () => {
-    Matter.World.addBody(world, ground);
-    Matter.World.addBody(world, colliderBottom);
-    Matter.World.addBody(world, colliderLeft);
-    Matter.World.addBody(world, colliderRight);
-
-    Matter.Runner.run(runner, engine);
-
-    this.init();
-  };
-
-  victoryAnimation = () => {
-    [
-      2, 2, -5, 5, -5, 2, -2, 5,
-      2, 2, -4, 5, -3, 2, -2, 2,
-      2, 2, -3, 5, -3, 2, -1, 0,
-    ].forEach((v, i) => {
-      setTimeout(() => this.setState({ 
-        personOffset: { top: styles.perssonHead.top + v },
-        personEyebrow: { top: styles.personEyebrow.top + v, transform: [ {rotate: i + 'deg' } ]}
-      }), 70 * i);
-    });
-  };
-
-  losingAnimation = () => {
-    [
-      0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 3, 3, 4, 5, 6
-    ].forEach((v, i) => {
-      setTimeout(() => this.setState({ 
-        personOffset: { top: styles.perssonHead.top + v },
-        personEyebrow: { top: styles.personEyebrow.top + v, transform: [ {rotate: (-i) + 'deg' } ]} 
-      }), 70 * i);
-    });
-  };
-
-  startAnimation = () => {
-    [
-      -1, -1, -1, -1, -1, -2, -2,
-      -2, -2, -2, -2, -3, -3, -3,
-      -3, -3, -4, -4, -4, -4, -4,
-      -5,
-    ].forEach((v, i) => {
-      setTimeout(() => this.setState({ 
-        personEyebrow: { top: styles.personEyebrow.top + v } 
-      }), 50 * i);
-    });
-  };
-
-  startup = () => {
-    setTimeout(() => {
-      this.startAnimation();
-    }, 2500);
-  };
-
-  updatePills = (remove = null, isPoint = false) => {
-    if (remove) {
-      this.removePill(remove);
-      world.gravity.y *= DELTASPEED;
-      if (isPoint) {
-        this.setState({ score: this.state.score + 1 });
-        this.playSound('coin');
-      } else {
-        this.playSound('break');
-      }
-    }
-    if (this.state.pillsLeft > 0) {
-      const newPill = makePill();
-      let leftColor = randomColor(),
-        rightColor = randomColor();
-      while (rightColor === leftColor) {
-        rightColor = randomColor();
-      }
-
-      Matter.Body.setVelocity(newPill, { x: 0, y: world.gravity.y / INITIALGRAVITY });
-      Matter.World.addBody(world, newPill);
-      Matter.Engine.update(engine);
-
-      this.setState({
-        pills: [...this.state.pills, { body: newPill, colors: { left: leftColor, right: rightColor } }],
-        pillsLeft: this.state.pillsLeft - 1
-      });
-    } else {
-      const newState = this.state.score >= WINNINGSCORE ? GAMESTATES.won : GAMESTATES.lost;
-      this.setState({
-        gameState: newState
-      });
-      if (newState === GAMESTATES.won) {
-        setTimeout(() => {
-          this.playEndingMusic('flagpole');
-          this.victoryAnimation();
-        }, 500);
-      } else {
-        setTimeout(() => {
-          this.playEndingMusic('die');
-          this.losingAnimation();
-        }, 400);
-      }
-    }
-  };
-
+  
   componentWillMount() {
-    // console.log(engine);
     Matter.Events.on(engine, 'collisionStart', (event) => {
       const pairs = event.pairs;
       if (pairs.length > 0) {
@@ -403,66 +135,17 @@ export default class App extends PureComponent {
         }
       }
     });
-    Matter.Events.on(engine, 'beforeUpdate', (ev) => {
-      this.setState({
-        pills: this.state.pills.map((p) => {
-          return {
-            ...p,
-            body: { ...world.bodies.find(b => b.id === p.body.id) }
-          }
-        })
-      });
-    });
-  }
 
-  sounds = {
-    break: { src: require('./assets/audio/break.wav'), obj: new Audio.Sound(), loaded: false, playing: false },
-    coin: { src: require('./assets/audio/coin.wav'), obj: new Audio.Sound(), loaded: false, playing: false },
-    theme: { src: require('./assets/audio/main.mp3'), obj: new Audio.Sound(), loaded: false, playing: false },
-    die: { src: require('./assets/audio/die.wav'), obj: new Audio.Sound(), loaded: false, playing: false },
-    flagpole: { src: require('./assets/audio/flagpole.wav'), obj: new Audio.Sound(), loaded: false, playing: false },
-    jump: { src: require('./assets/audio/jump.wav'), obj: new Audio.Sound(), loaded: false, playing: false },
-  };
-
-  async playSound(name, pause = null, loop = false, volume = 0.9) {
-    if (this.sounds[name].playing) {
-      return;
-    }
-    if (pause) {
-      await this.pauseSound(pause);
-    } 
-    try {
-      this.sounds[name].playing = true;
-      if (!this.sounds[name].loaded) {
-        await this.sounds[name].obj.loadAsync(this.sounds[name].src);
-        await this.sounds[name].obj.setVolumeAsync(volume);
-        await this.sounds[name].obj.setIsLoopingAsync(loop);
-        this.sounds[name].loaded = true;
-      }
-      await this.sounds[name].obj.playFromPositionAsync(0);  
-      this.sounds[name].playing = false;
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  async pauseSound(name) {
-    if (!this.sounds[name].loaded) {
-      return;
-    }
-    try {
-      await this.sounds[name].obj.pauseAsync();
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  async playMainTheme() {
-    await this.playSound('theme', null, true, 0.5);
-  };
-
-  async playEndingMusic(sound = 'die') {
-    await this.playSound(sound, null, false, 1.0);
+    // Matter.Events.on(engine, 'beforeUpdate', (ev) => {
+    //   this.setState({
+    //     pills: this.state.pills.map((p) => {
+    //       return {
+    //         ...p,
+    //         body: { ...world.bodies.find(b => b.id === p.body.id) }
+    //       }
+    //     })
+    //   });
+    // });
   }
 
   async componentDidMount() {
@@ -470,18 +153,119 @@ export default class App extends PureComponent {
       'space-grotesk-bold': require('./assets/fonts/SpaceGrotesk-Bold.ttf'),
       'space-grotesk-regular': require('./assets/fonts/SpaceGrotesk-Regular.ttf')
     });
-    await this.playMainTheme();
+
+    await this.soundPlayer.play('theme');
+
     this.setState({ fontsLoaded: true });
-    this.startup();
+
+    setTimeout(() => this.started(), 3000);
   }
 
+  
+  init = () => {
+    world.gravity.y = INITIALGRAVITY;
+    this.setState({
+      gameState: GAMESTATES.started,
+      pills: [],
+      pillsLeft: MAXSCORE,
+      score: 0
+    });
+
+    setTimeout(this.updatePills, 250);
+  };
+
+  removePill = (body) => {
+    this.setState({ pills: this.state.pills.filter(p => p.body.id !== body.id) });
+    Matter.World.remove(world, body);
+  };
+
+
+  updatePills = (remove = null, isPoint = false) => {
+    if (remove) {
+      this.removePill(remove);
+      world.gravity.y *= DELTASPEED;
+      if (isPoint) {
+        this.setState({ score: this.state.score + 1 });
+        this.soundPlayer.play('coin');
+      } else {
+        this.soundPlayer.play('break');
+      }
+    }
+    if (this.state.pillsLeft > 0) {
+      const newPill = makePill();
+      const colors = randomColors(2);
+
+      Matter.Body.setVelocity(newPill, { x: 0, y: world.gravity.y / INITIALGRAVITY });
+      Matter.World.addBody(world, newPill);
+      Matter.Engine.update(engine);
+
+      this.setState({
+        pills: [...this.state.pills, { body: newPill, colors: { left: colors[0], right: colors[1] } }],
+        pillsLeft: this.state.pillsLeft - 1
+      });
+    } else { // game ended
+      const newState = this.state.score >= WINNINGSCORE ? GAMESTATES.won : GAMESTATES.lost;
+
+      this.setState({ gameState: newState });
+
+      if (newState === GAMESTATES.won) {
+        setTimeout(() => { this.victory() }, 1000);
+      } else {
+        setTimeout(() => { this.lost() }, 1000);
+      }
+    }
+  };
+
+  // Animations:
+  victory = () => {
+    this.soundPlayer.play('flagpole', true);
+
+    [
+      2, 2, -5, 5, -5, 2, -2, 5,
+      2, 2, -4, 5, -3, 2, -2, 2,
+      2, 2, -3, 5, -3, 2, -1, 0,
+    ].forEach((v, i) => {
+      setTimeout(() => this.setState({ 
+        personOffset: { top: styles.perssonHead.top + v },
+        personEyebrow: { top: styles.personEyebrow.top + v, transform: [ {rotate: i + 'deg' } ]}
+      }), 70 * i);
+    });
+  };
+
+  lost = () => {
+    this.soundPlayer.play('die', true);
+
+    [
+      0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 3, 3, 4, 5, 6
+    ].forEach((v, i) => {
+      setTimeout(() => this.setState({ 
+        personOffset: { top: styles.perssonHead.top + v },
+        personEyebrow: { top: styles.personEyebrow.top + v, transform: [ {rotate: (-i) + 'deg' } ]} 
+      }), 70 * i);
+    });
+  };
+
+  started = () => {
+    [
+      -1, -1, -1, -1, -1, -2, -2,
+      -2, -2, -2, -2, -3, -3, -3,
+      -3, -3, -4, -4, -4, -4, -4,
+      -5,
+    ].forEach((v, i) => {
+      setTimeout(() => this.setState({ 
+        personEyebrow: { top: styles.personEyebrow.top + v } 
+      }), 50 * i);
+    });
+  };
+
+  // Subcomponents
   startscreen = () => {
     return !this.state.fontsLoaded ? <Text>Loading...</Text> : (
       <View style={[styles.text, { top: HEIGHT / 3 }]}>
         <Text style={{ textAlign: 'center', fontSize: 48, fontFamily: 'space-grotesk-bold' }}>
           dr. persson
           </Text>
-        <TouchableHighlight onPress={this.start}>
+        <TouchableHighlight onPress={this.init}>
           <View style={{ backgroundColor: '#fff', padding: 15, paddingBottom: 20, margin: 15 }}>
             <Text style={{ textAlign: 'center', fontSize: 32, fontFamily: 'space-grotesk-regular' }}>
               start game
@@ -523,47 +307,28 @@ export default class App extends PureComponent {
     );
   };
 
-  styleForBody = (body) => {
-    var obj = world.bodies.find(b => b.id === body.id);
-    if (obj && obj.position) {
-      return {
-        position: 'absolute',
-        top: obj.position.y * HEIGHT,
-        left: obj.position.x,
-        width: obj.width,
-        height: obj.height
-      };
-    }
-    return {};
-  }
-
-  bodyToView = (body) => {
-    return { position: 'absolute', backgroundColor: '#f00' };
-  }
-
   render() {
-    const state = this.state.gameState,
-      gameover = state === GAMESTATES.won || state === GAMESTATES.lost;
+    const state = this.state.gameState;
+
     return (
       <GameLoop style={styles.container} onUpdate={this.updateHandler}>
         {state === GAMESTATES.started ? null :
           <Persson offset={this.state.personOffset} eyebrow={this.state.personEyebrow} />
         }
+
         {state !== GAMESTATES.init ? null : this.startscreen()}
-        {!gameover ? null : this.gameover()}
+
+        {!(state === GAMESTATES.won || state === GAMESTATES.lost) ? null : this.gameover()}
+
         {state !== GAMESTATES.started ? null : this.scoreboard()}
+        
         {this.state.pills.map((p, i) => {
-          return (<Pill key={'pill-' + i} style={{ transform: [{ rotate: p.body.angle + 'rad' }] }} body={p.body} colors={p.colors} />);
+          return (<Pill key={'pill-' + i} body={p.body} colors={p.colors} />);
         })}
 
-        {state !== GAMESTATES.started ? null :
-          <Beaker />
-        }
-        {state !== GAMESTATES.started ? null :
-          <View style={{ position: 'absolute', left: 0, bottom: 0, width: WIDTH, height: styles.beaker.bottom - 5, backgroundColor: '#fff' }} />
-        }
+        {state !== GAMESTATES.started ? null : <Beaker /> }
+        {state !== GAMESTATES.started ? null : <Ground /> }
       </GameLoop>
     );
   }
 }
-// WIDTH / 2, HEIGHT - 5, WIDTH * 3, 10
